8dae632bb32ff1c2a693626356a41438837c2159
Furkan Yıldız <65228266+furkanyldz007@users.noreply.github.com>
2021-07-20 20:58:45 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js")
+const client = new Discord.Client()
+const logs = require('discord-logs');
+logs(client);
+const ayarlar = require("./ayarlar.json")
+
+client.on("guildMemberOffline", (member, oldStatus) => {
+  if(member.id !== "botid") return
+offlinelog = client.channels.cache.get('oflinelogkanalid')
+
+let embed = new Discord.MessageEmbed()
+.setColor("RED")
+.setTitle("FuTi Uptime Log")
+.setDescription(member.user.tag+ "Düştü \nNedeni: Teknik Bakım.")
+.setTimestamp()
+offlinelog.send(embed)
+});
+
+client.on("guildMemberOnline", (member, newStatus) => {
+  if(member.id !== "botid") return
+onlinelog = client.channels.cache.get('onlinelogkanalid')
+let embed = new Discord.MessageEmbed()
+.setColor("GREEN")
+.setTitle("FuTi Uptime Log")
+.setDescription(member.user.tag+ "Tekrardan Aktif. ")
+.setTimestamp()
+onlinelog.send(embed)
+});
+
+
+client.login(ayarlar.token);
